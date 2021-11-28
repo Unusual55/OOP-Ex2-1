b435bcc12b332a7b60d6b927efac4de2cc7a89db
Ofri Tavor <ofritavor55@gmail.com>
2021-11-28 12:21:03 +0200

    Created the test class

---
+++ Ex2/src/api/Tests/Point3dTest.java
@@ -0,0 +1,12 @@
+package api.Tests;
+
+import api.Point3D;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Point3dTest {
+    @Test
+    void Constructors(){
+
+    }
+}
